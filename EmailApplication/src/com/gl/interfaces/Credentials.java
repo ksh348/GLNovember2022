4effe66868497ed18da0359e046be9c4c6cf89df
Sridhar Kokkula <107347714+ksh348@users.noreply.github.com>
2022-12-04 13:35:22 +0530

    Delete EmailApplication directory

--- EmailApplication/src/com/gl/interfaces/Credentials.java
+++
@@ -1,9 +0,0 @@
-package com.gl.interfaces;
-
-import com.gl.entities.Employee;
-
-public interface Credentials {
-	String generatePassword();
-	String generateEmailAddress(String firstName, String lastName, String department);
-	void showCredentials(Employee emp);
-}
